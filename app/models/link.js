00b7b0fe86dd7a01b9b219dbff6c60259241fe74
Jarrett Gliner <jarrett.gliner@gmail.com>
2016-02-11 16:44:47 -0800

    remove comment in link.js

--- app/models/link.js
+++ app/models/link.js
@@ -9,20 +9,5 @@ db.linkSchema.pre('save', function(next) {
   next();
 });
 
-// var Link = db.Model.extend({
-//   tableName: 'urls',
-//   hasTimestamps: true,
-//   defaults: {
-//     visits: 0
-//   },
-//   initialize: function() {
-//     this.on('creating', function(model, attrs, options) {
-//       var shasum = crypto.createHash('sha1');
-//       shasum.update(model.get('url'));
-//       model.set('code', shasum.digest('hex').slice(0, 5));
-//     });
-//   }
-// });
-
 var Link = mongoose.model('Link', db.linkSchema);
 module.exports = Link;
--- app/models/user.js
+++ app/models/user.js
@@ -22,23 +22,3 @@ db.userSchema.statics.comparePassword = function(attemptedPassword, hashedPasswo
 
 var User = mongoose.model('User', db.userSchema);
 module.exports = User;
-
-// var User = db.Model.extend({
-//   tableName: 'users',
-//   hasTimestamps: true,
-//   initialize: function() {
-//     this.on('creating', this.hashPassword);
-//   },
-//   comparePassword: function(attemptedPassword, callback) {
-//     bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
-//       callback(isMatch);
-//     });
-//   },
-//   hashPassword: function() {
-//     var cipher = Promise.promisify(bcrypt.hash);
-//     return cipher(this.get('password'), null, null).bind(this)
-//       .then(function(hash) {
-//         this.set('password', hash);
-//       });
-//   }
-// });
